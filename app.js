b96eb9e4decbc917cd4b00abfe64dd1c303f8003
Leslie Lopez <leslie2515lopez@gmail.com>
2022-05-08 21:28:10 -0500

    Added function for scrollup

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // SHOW MENU
 const navMenu = document.getElementById("nav-menu"),
-      navToggle = document.getElementById("nav-toggle"),
-      navClose = document.getElementById("nav-close");
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
 // MENU SHOW
 /* Validate if constant exists */
-if(navToggle){
+if (navToggle) {
     navToggle.addEventListener("click", () => {
         navMenu.classList.add("show-menu");
     });
@@ -13,8 +13,8 @@ if(navToggle){
 
 // MENU HIDDEN
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener("click", () =>{
+if (navClose) {
+    navClose.addEventListener("click", () => {
         navMenu.classList.remove("show-menu");
     });
 }
@@ -22,7 +22,7 @@ if(navClose){
 // REMOVE MENU MOBILE
 const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById("nav-menu");
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove("show-menu");
@@ -30,9 +30,19 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener("click", linkAction));
 
 // CHANGE BACKGROUND HEADER
-function scrollHeader(){
+function scrollHeader() {
     const header = document.getElementById("header");
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add("scroll-header"); else header.classList.remove("scroll-header");
+    if (this.scrollY >= 50) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
+
+// SHOW SCROLL UP
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
